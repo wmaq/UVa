81fa33b1410b707b72d494e2b32da8bc11721e8c
wmaq <wojciechmakowski7@gmail.com>
2015-03-31 11:22:38 +0200

    fixed error

--- Stern-Broccot/stern.c
+++ Stern-Broccot/stern.c
@@ -8,6 +8,9 @@ int main(void){
     int x,y;
     while (scanf("%d %d",&x,&y)){
         if (x==1 && y==1) break;
+        left.a=0; left.b=1;
+        mid.a=1; mid.b=1;
+        right.a=1; right.b=0;
         while(1){
             long double temp1 = (long double)x/y;
             long double temp2 = (long double)mid.a/mid.b;
