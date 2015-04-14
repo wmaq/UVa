bd1ac497ca54e8e025321aebfdd6d747a6f56ae8
wmaq <wojciechmakowski7@gmail.com>
2015-04-14 14:08:10 +0200

    fixed wrong commit

--- Is Bigger Smarter/ibs.cpp
+++ Is Bigger Smarter/ibs.cpp
@@ -1,5 +1,5 @@
 #include <cstdio>
-#include <iostream>
+#include <cstring>
 #include <algorithm>
 using namespace std;
 
@@ -14,12 +14,29 @@ struct T{
 
 int main(void){
 	struct T eleph[1001];
-	int p = 0;
-	while( scanf("%d %d", &eleph[p].A, &eleph[p].B){
+    int arr[1001];
+    int temp[1001];
+	int p = 0, no1 = 0, x = 0;
+	while( scanf("%d %d", &eleph[p].A, &eleph[p].B) == 2){
 		eleph[p].n = p+1;
 		p++;
 	}
 	sort(eleph, eleph + p);
-	
+	memset(arr, -1, sizeof(arr));
+    for(int i = 0; i < p; i++){
+        temp[i] = 1;
+        for(int j = 0; j < i; j++){
+            if(eleph[j].B < eleph[i].B && eleph[j].A > eleph[i].A && temp[j]+1 > temp[i]){
+                temp[i] = temp[j] + 1;
+                arr[i] = j;
+            }
+        }
+        if(temp[i] > no1){
+            no1 = temp[i];
+            x = i;
+        }
+    }
+    printf("%d\n", no1);
+    for(int i = x; i!=-1 ; i = arr[i]) printf("%d\n", eleph[i].n);
 	return 0;
 }
